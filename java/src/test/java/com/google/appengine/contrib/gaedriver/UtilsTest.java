c3463cbaec16cc0103f27490037b02493da394a6
Robert Schuppenies <schuppe@google.com>
2012-11-29 07:52:21 -0800

    Explicitly mark tests as JUnit4 tests.

--- java/src/test/java/com/google/appengine/contrib/gaedriver/UtilsTest.java
+++ java/src/test/java/com/google/appengine/contrib/gaedriver/UtilsTest.java
@@ -1,30 +1,32 @@
 /*
  * Copyright (C) 2012 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 
 package com.google.appengine.contrib.gaedriver;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * Unit tests for Utils.
  *
  * @author schuppe@google.com (Robert Schuppenies)
  */
+@RunWith(JUnit4.class)
 public class UtilsTest {
 
   @Test
